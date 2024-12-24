3ab853d126f70f8e9af9d30914a4230468d9e9b0
kadersutlu <sutlukader@gmail.com>
2024-12-24 21:59:51 +0300

    Implemented customer login functionality

--- app/src/main/java/com/example/glamoraapp/ui/fragment/customer/login/CustomerLoginFragment.kt
+++ app/src/main/java/com/example/glamoraapp/ui/fragment/customer/login/CustomerLoginFragment.kt
@@ -1,10 +1,15 @@
 package com.example.glamoraapp.ui.fragment.customer.login
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.example.glamoraapp.R
 import com.example.glamoraapp.databinding.FragmentCustomerLoginBinding
 import com.example.glamoraapp.ui.BaseFragment
+import com.example.glamoraapp.ui.activity.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,7 +17,80 @@ class CustomerLoginFragment :
     BaseFragment<FragmentCustomerLoginBinding, CustomerLoginViewModel>() {
 
     override fun initView() {
+        loginButtonAction()
 
+        forgetPasswordAction()
+
+        loginSuccessAction()
+
+        resetPasswordAction()
+    }
+
+    private fun loginButtonAction() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailInput.editText?.text.toString()
+            val password = binding.passwordInput.editText?.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                viewModel.signInUser(email, password)
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.error_empty_fields),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun forgetPasswordAction() {
+        binding.forgotPasswordText.setOnClickListener {
+            val email = binding.emailInput.editText?.text.toString()
+            if (email.isNotEmpty()) {
+                viewModel.resetPassword(email)
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.error_invalid_email),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun loginSuccessAction() {
+        viewModel.loginSuccess.observe(viewLifecycleOwner) { isSuccess ->
+            if (isSuccess) {
+                val sharedPreferences =
+                    requireActivity().getSharedPreferences("AppPrefs", Context.MODE_PRIVATE)
+                sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
+
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.login_failed), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun resetPasswordAction() {
+        viewModel.resetPasswordSuccess.observe(viewLifecycleOwner) { isSuccess ->
+            if (isSuccess) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.reset_password_email_sent),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.reset_password_failed), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        viewModel.errorMessage.observe(viewLifecycleOwner) { errorMessage ->
+            Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override val viewModel by viewModels<CustomerLoginViewModel>()
--- app/src/main/java/com/example/glamoraapp/ui/fragment/customer/login/CustomerLoginViewModel.kt
+++ app/src/main/java/com/example/glamoraapp/ui/fragment/customer/login/CustomerLoginViewModel.kt
@@ -1,9 +1,51 @@
 package com.example.glamoraapp.ui.fragment.customer.login
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.glamoraapp.R
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @HiltViewModel
-class CustomerLoginViewModel @Inject constructor() : ViewModel() {
+class CustomerLoginViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    @ApplicationContext private val context: Context
+) : ViewModel() {
+
+    private val _loginSuccess = MutableLiveData<Boolean>()
+    val loginSuccess: LiveData<Boolean> get() = _loginSuccess
+
+    private val _resetPasswordSuccess = MutableLiveData<Boolean>()
+    val resetPasswordSuccess: LiveData<Boolean> get() = _resetPasswordSuccess
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> get() = _errorMessage
+
+    fun signInUser(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _loginSuccess.value = true
+                } else {
+                    _loginSuccess.value = false
+                    _errorMessage.value = context.getString(R.string.login_error_failed)
+                }
+            }
+    }
+
+    fun resetPassword(email: String) {
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _resetPasswordSuccess.value = true
+                } else {
+                    _resetPasswordSuccess.value = false
+                    _errorMessage.value = context.getString(R.string.reset_password_error_failed)
+                }
+            }
+    }
 }
