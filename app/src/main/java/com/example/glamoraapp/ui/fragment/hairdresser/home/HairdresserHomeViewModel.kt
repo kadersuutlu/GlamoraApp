9e008176f4569d44b50ff1d67f32e46da1850b44
kadersutlu <sutlukader@gmail.com>
2025-01-08 21:13:34 +0300

    create hairdresser home fragment

---
+++ app/src/main/java/com/example/glamoraapp/ui/fragment/hairdresser/home/HairdresserHomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.glamoraapp.ui.fragment.hairdresser.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HairdresserHomeViewModel @Inject constructor() : ViewModel() {
+}
