2a7b3b92a1e10893a0fc4dbfbd9aa4e31222ffbc
kadersutlu <sutlukader@gmail.com>
2025-01-08 21:13:02 +0300

    create customer management fragment

---
+++ app/src/main/java/com/example/glamoraapp/ui/fragment/hairdresser/customer_management/CustomerManagementViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.glamoraapp.ui.fragment.hairdresser.customer_management
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CustomerManagementViewModel @Inject constructor() : ViewModel() {
+}
