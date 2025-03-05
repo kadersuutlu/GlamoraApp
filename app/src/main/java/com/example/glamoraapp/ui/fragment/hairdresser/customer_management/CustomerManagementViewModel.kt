f516dcd9c3314f33a0248979850c73e93209f45d
kadersutlu <sutlukader@gmail.com>
2025-03-05 22:18:40 +0300

    hairdresser ui

--- app/src/main/java/com/example/glamoraapp/ui/fragment/hairdresser/customer_management/CustomerManagementViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.glamoraapp.ui.fragment.hairdresser.customer_management
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class CustomerManagementViewModel @Inject constructor() : ViewModel() {
-}
